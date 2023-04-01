bcc50d3a682f7e61272a2a57dd71d3ea8cdce301
wojtko <j.w2001@onet.pl>
2023-04-01 23:36:02 +0200

    Create stateful constant to store note object

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 function CreateArea(){
+
+    const [note, setNote] = React.useState({
+        title: "",
+        content: ""
+    });
+
     return (
         <div>
             <form>
